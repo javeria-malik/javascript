1386ccd9f51247af4d1fec09f1d346fbb4655e93
Javeria Malik <javeriamalik6692@gmail.com>
2023-07-07 18:06:00 +0500

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,53 @@
+document.write("waste of time")
+var age=25;
+var city="fsd";
+alert (age)
+console.log(city)
+var a;
+a=68
+alert(a)
+var b="its all'right if everything "
+console.log(b)
+var a=88;
+var b=90;
+var sum=a+b;
+console.log(sum)
+var isfeepaid=true;
+console.log(isfeepaid)
+var a=null;
+console.log(a)
+a=9;
+console.log(a)
+console.log(typeof(a));
+var a=6; var b=9; var c=a+b;
+console.log(a);console.log(b);console.log(c);
+var my_name="jiya";
+console.log(my_name);
+var my$name="jiya";
+console.log(my$name);
+var $myname="jiya";
+console.log($myname);
+//hello this is the method of single line comment
+/* this was the method of multiline comment 
+keep it in mind */
+//var var=7:// cant be exucuted give error
+//var for=9// will also give error
+//camel case:in this during giving variable name you will write multiple wprds without space as this way
+var userRespond="fake"
+alert(userRespond)
+var a=7;
+var b =9;
+var c=a+b;
+var d=a-b;
+var e=a*b;
+var f=a/b;
+var g=a%b;
+var h=a**b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+
+

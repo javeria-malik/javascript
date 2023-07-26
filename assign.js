f306b0c9414878b465d1fca5715eedfb984536c9
Javeria Malik <javeriamalik6692@gmail.com>
2023-07-26 16:07:11 +0500

    Add files via upload

---
+++ assign.js
@@ -0,0 +1,81 @@
+alert("welcome");
+/*var secretnumber=123456;
+var userguess=parseInt(prompt("guess the number range from 1 to 10"));
+if (secretnumber===userguess) {
+    alert("Bingo! correct answer");
+}else if(secretnumber===userguess+1){
+    alert("close enough to correct answer");
+}*/
+// QUESTION NO 2
+/*Write a program that checks whether the given input is an even
+number or an odd number
+var userguess=parseInt(prompt("enter your desire  number"));
+if (userguess%2==0){
+    console.log("number is even");
+} else {
+    console.log("number is odd");
+}*/
+//Question no 3
+/*Write a program to create a calculator for +,-,*, / & % using if
+statements. Take the following input:
+a. First number
+b. Second number
+c. Operation (+, -, *, /, %)
+Compute & show the calculated result to user.
+ */
+/*console.log("calculate")
+var userinput1=parseInt(prompt("enter first number"));
+var userinput2=parseInt(prompt("enter second number"));
+var operation=prompt("enter your desire operation");
+if (operation=="+"){
+    console.log(userinput1+userinput2);
+}else if (operation=="-"){
+    console.log(userinput1-userinput2);
+}else if (operation=="*"){
+    console.log(userinput1*userinput2);
+}else if (operation=="/"){
+    console.log(userinput1/userinput2);
+}else if (operation=="%"){
+    console.log(userinput1%userinput2);
+}*/
+/*Write a program that takes input a number from user & state whether
+the number is positive, negative or zero.*/
+/*var userinput=parseInt(prompt("enter number"));
+if (userinput>0) {
+    document.write("your number is positive");
+}else if (userinput<0) {
+    document.write("your number is negative");
+}*/
+//Write a program that takes a character (i.e. string of length 1) and
+//returns true if it is a vowel, false otherwise
+/*var userinput=prompt("enter your string have len 1").toLowerCase();
+if (userinput==="a"||userinput==="e"||userinput==="i"||userinput==="o"||userinput==="u"){
+    document.write("true")
+}else{
+    document.write("fasle")
+}*/
+/* Write a program that
+a. Store correct password in a JS variable.
+b. Asks the user to enter his/her password
+c. Validate the two passwords:
+i. Check if user has entered password. If not, then give the
+message “ Please enter your password”
+ii. Check if both passwords are the same. If they are the
+same, show message “Correct! The password you
+entered matches the original password”. Show “Incorrect
+password” otherwise.*/
+/*var password="abcdef12";
+var userpassword=prompt("enter password");
+if(userpassword===""){
+    document.write("Please enter your password");
+}else if (password==userpassword){
+    document.write("Correct! The password you entered matches the original password")
+}else {
+    document.write("incorrect password")
+}*/
+
+
+
+
+
+

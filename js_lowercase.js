2ef4342ddd467ee961c770b6d31000bb150dec3c
Javeria Malik <javeriamalik6692@gmail.com>
2023-08-04 17:19:59 +0500

    Add files via upload

---
+++ js_lowercase.js
@@ -0,0 +1,11 @@
+var food="pizza";
+var update="Pizza";
+if(food.toLowerCase() === update.toLowerCase()){
+    document.write("match<br>");
+}
+else{
+    document.write("not match<br>");
+}
+alert("welcome ");
+document.write(food.toUpperCase(),"<br>");
+document.write(food.toLowerCase());

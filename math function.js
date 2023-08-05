daa87a90f7434a98b6e6c8acd24af4f3da5a820a
Javeria Malik <javeriamalik6692@gmail.com>
2023-08-05 15:25:05 +0500

    Add files via upload

---
+++ math function.js
@@ -0,0 +1,25 @@
+//math .round functions 
+//use to round the values
+var a=Math.round(4.7);
+document.write(a,"<br>");
+// math.ceil function use to round to upper side
+var a=Math.ceil(4.7);
+document.write(a,"<br>");
+//math.floor function use to round to down side
+var a=Math.floor(4.7);
+document.write(a,"<br>");
+//math.random use to enerate the random number 
+var a=Math.random();
+document.write(a,"<br>");
+// if you want to give range in random function we use........ sine want to give ramge from 1 to  6
+var a=Math.random();
+document.write((a*6)+1,"<br>");// multiply the ending number of range 
+var a=Math.random();
+document.write((a*9)+1,"<br>");
+var a=Math.random();
+document.write((a*9)+1,"<br>");
+// toFixedFLOOR............ fixed the floor values to specifed function 
+ var average=(23+22+26)/3;
+ document.write(average,"<br>");
+ document.write(average.toFixed(3));// here fixed value to 3
+ 

3d510144c83bf391957aa8c2fe3b8c8c189d82fa
Javeria Malik <javeriamalik6692@gmail.com>
2023-08-05 15:49:17 +0500

    Add files via upload

---
+++ date objects.js
@@ -0,0 +1,10 @@
+//date use to generate the date
+var date=new Date();
+document.write(date)// it will gernettaed the standard date of system or server where we are working for 
+// we can give our desire format for date objects 
+//new Date()
+//var date=new Date(year,month,day,hours,minutes,seconds,milliseconds);
+//document.write(date);
+var date=new Date(year);
+alert(date);
+alert("welcome");
